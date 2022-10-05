27487f098285b8f9de97d5da537a53db53e6012b
Ramon Moura <ramonmoura18@gmail.com>
2022-10-05 10:16:03 -0300

    Tratamento do formulário de Login e aperfeiçoamento de interações

--- game_mania_angular/src/app/login/login.component.ts
+++ game_mania_angular/src/app/login/login.component.ts
@@ -10,14 +10,34 @@ import { LoginService } from '../services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
   userModel = new User();
+  msgError = '';
   receberDados() {
     this.loginService.login(this.userModel).subscribe((response) => {
-      console.log(response);
+      // console.log(response);
+      this.router.navigateByUrl('/');
+    }, (error) => {
+      switch (error.error) {
+        case "Email and password are required":
+          this.msgError = "Email e senha são obrigatórios";
+          break;
+          case "Email format is invalid":
+            this.msgError = "Formato de email inválido";
+            break;
+          case "Cannot find user":
+            this.msgError = "Usuário não encontrado";
+            break;
+          case "Incorrect password":
+            this.msgError = "Senha incorreta";
+            break;
+
+        default:
+          this.msgError = error.error
+      }
     });
     // console.log(this.userModel);
   }
